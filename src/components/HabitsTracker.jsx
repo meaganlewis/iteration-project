7d3871b2660c93b8f6a6c14d60b2c91997bdeda0
Richard Rivera <richard.rivera.roman@gmail.com>
2025-01-16 16:03:52 -0500

    Change button to Link component

--- src/components/HabitsTracker.jsx
+++ src/components/HabitsTracker.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import '../styles/habitsTracker.css';
 import UserEntries from './card';
+import { Link } from 'react-router-dom';
 
 export default function HabitsTracker() {
   const [active, setActive] = useState('tab-1');
@@ -13,20 +14,22 @@ export default function HabitsTracker() {
     <div>
       <div className='container'>
         <div className='tabs'>
-          <button
-            className={`tab-button ${active === 'tab-1' ? 'active' : ''}`}
-            id='tab-1'
-            onClick={handleClick}
+          <Link
+            to='/habits'
+            className={`tab-button ${
+              active === 'tab-1' ? 'active' : ''
+            } id='tab-1' onClick={handleClick}`}
           >
             Healthy Habits
-          </button>
-          <button
+          </Link>
+          <Link
+            to='/history'
             className={`tab-button ${active === 'tab-2' ? 'active' : ''}`}
             id='tab-2'
             onClick={handleClick}
           >
             History
-          </button>
+          </Link>
         </div>
         <div className='tabs-content'>
           <div className={`tab-page ${active === 'tab-1' ? 'active' : ''}`}>
